f209e118f32e5c720a78206466737716e56e2f12
Ricky <ricky@Ricky-PC.(none)>
2010-04-28 16:30:45 +0800

    Add HTTP Request method

--- php/EventHttpServer.class.php
+++ php/EventHttpServer.class.php
@@ -72,6 +72,7 @@ abstract class EventHttpServer {
                 'MaxConnected'=>1000,
                 'ClientSocketTTL'=>60,
                 'ClientSocketTTLPool'=>30,
+                'BaseDir'=>'./web',
         );
         $this->Config=array_merge($DefaultConfig,$Config);
         $this->ChunkDataHeader = "HTTP/1.1 200 OK\r\n"
@@ -163,12 +164,25 @@ abstract class EventHttpServer {
         $this->CurrentConnected++;
         echo "Client " . stream_socket_get_name($ClientSocket,true) . " connected.\n";
         stream_set_blocking($ClientSocket,0);
+        /*
+        $TimeoutEvent=event_new();
+        //event_set($TimeoutEvent,-1,EV_TIMEOUT,array($this,'TimeoutEvent'));
+        echo "Timeout:";
+        print_r($TimeoutEvent);
+        echo event_timer_set($TimeoutEvent,array($this,'TimeoutEvent'));
+        echo event_timer_pending($TimeoutEvent,5000000);
+        echo event_base_set($TimeoutEvent,$this->BaseEvent);
+        echo event_add($TimeoutEvent);
+        */
         $BufferEvent=event_buffer_new($ClientSocket,array($this,'doReceive'),array($this,'WriteFin'),array($this,'OnClientBufferError'),$ClientSocket);
         event_buffer_watermark_set($BufferEvent,EV_WRITE,1,1);
         event_buffer_base_set($BufferEvent,$this->BaseEvent);
         event_buffer_enable($BufferEvent,EV_READ|EV_WRITE);
         event_add($BufferEvent);
         $this->ClientQueueAdd($ClientSocket,$BufferEvent);
+    }
+    public function TimeoutEvent() {
+        echo "Got Timeout\n";
     }
     private function ClientQueueAdd($ClientSocket,$Event) {
         $SocketName=stream_socket_get_name($ClientSocket,true);
@@ -184,13 +198,11 @@ abstract class EventHttpServer {
     }
     protected function ClientInit($ClientSocket) {
         $SocketName=stream_socket_get_name($ClientSocket,true);
-        $Data=$this->ClientData[$SocketName];
-        $Data['Init']=true;
+        $this->ClientData[$SocketName]['Init']=true;
     }
     private function IsClientInit($ClientSocket) {
         $SocketName=stream_socket_get_name($ClientSocket,true);
-        $Data=$this->ClientData[$SocketName];
-        return $Data['Init'];
+        return $this->ClientData[$SocketName]['Init'];
     }
     private function ClientQueueRemove($SocketName) {
         $Data=$this->ClientData[$SocketName];
@@ -213,11 +225,11 @@ abstract class EventHttpServer {
         echo "Client " . stream_socket_get_name($ClientSocket,true) . " disconnect.\n";
     }
     private function extraceHttpHeader($RawHeaders) {
-        foreach($RawHeaders as $RawHeader) {
+        foreach($RawHeaders as $Index => $RawHeader) {
             $RawHeader=trim($RawHeader);
-            if(stripos($RawHeader,'GET')===0) {
+            if($Index==0) {
                 list($Method,$File,$Protocal)=explode(' ',$RawHeader);
-                $HttpHeaders[strtolower($Method)]=array($File,$Protocal);
+                $HttpHeaders['_Request_']=array('Method'=>strtoupper(trim($Method)),'File'=>trim($File),'Protocal'=>trim($Protocal));
                 continue;
             }
             list($Key,$Data)=explode(':',$RawHeader);
@@ -253,17 +265,95 @@ abstract class EventHttpServer {
             $data=str_replace("\r",'',$data);
             $this->extraceHttpHeader(explode("\n",$data));
             $this->ClientInit($ClientSocket);
-            event_buffer_write($BufferEvent,$this->ChunkDataHeader);
+            $this->ProcessRequest($BufferEvent,$SocketName);
         }
+    }
+    private function ProcessRequest($BufferEvent,$SocketName) {
+        $Request=$this->HttpHeaders['_Request_'];
+        switch($Request['Method']) {
+            case 'GET':
+            case 'POST':
+            case 'PUT':
+            case 'DELETE':
+                if(preg_match('/^\/comet\/?$/i',$Request['File'])) {
+                    echo "comet\n";
+                    event_buffer_write($BufferEvent,$this->ChunkDataHeader);
+                    return;
+                }
+                break;
+            default:
+                $this->ClientShutDown($SocketName);
+                return;
+        }
+        echo "output file\n";
+        $this->OutputFile($BufferEvent,$SocketName);
+    }
+    private function MimeInfo($File){
+        $Info = pathinfo($File);
+        switch(strtolower($Info['basename'])){
+            case 'html':
+            case 'htm':
+                return 'text/html';
+            case 'txt':
+            case 'js':
+                return 'text/plain';
+            case 'jpg':
+            case 'jpeg':
+                return 'image/jpeg';
+            case 'gif':
+                return 'image/gif';
+            case 'png':
+                return 'image/png';
+            case 'gif':
+                return 'image/gif';
+            case 'swf':
+                return 'application/x-shockwave-flash';
+        }
+    }
+    public function OutputFile($BufferEvent,$SocketName){
+        $Request=$this->HttpHeaders['_Request_'];
+        $File=preg_replace('/\.{2,}/','',substr($Request['File'],1));
+        $File=getcwd().'/'.$this->Config['BaseDir']."/$File";
+        if(!file_exists($File))
+         return $this->Send404Error($BufferEvent,$SocketName);
+        $Data="HTTP/1.1 200\r\n"
+             ."Content-Type: ".$this->MimeInfo($File)."\r\n"
+             ."Connection: close\r\n"
+             ."Content-Length: ".filesize($File)."\r\n\r\n";
+        echo $Data;
+        event_buffer_write($BufferEvent,$Data);
+        $hFile=fopen($File,'r');
+        while(!feof($hFile)){
+          $Data=fread($hFile,4096);
+          event_buffer_write($BufferEvent,$Data);  
+        }
+        fclose($hFile);
+        $this->ClientData[$SocketName]['DataFin']=true;
     }
     public function WriteFin($BufferEvent,$ClientSocket) {
         $SocketName=stream_socket_get_name($ClientSocket,true);
         if($this->ClientData[$SocketName]['Fin']) {
             event_buffer_free($BufferEvent);
             $this->ClientShutDown($SocketName);
+            return;
+        }
+        if($this->ClientData[$SocketName]['DataFin']){
+            event_buffer_free($BufferEvent);
+            $this->ClientShutDown($SocketName,false);
+            return;
         }
     }
-    public function OnClientBufferError($BufferEvent ,$ClientSocket) {
+    private function Send404Error($BufferEvent,$SocketName){
+        $Message="File not found!";
+        $Data="HTTP/1.0 404 Not Found\r\n"
+             ."Content-Type: text/plain; charset=UTF-8\r\n"
+             ."Connection: close\r\n"
+             ."Content-Length: ".strlen($Message)."\r\n\r\n$Message";
+        event_buffer_write($BufferEvent,$Data);
+        $this->ClientData[$SocketName]['DataFin']=true;
+        return true;
+    }
+    public function OnClientBufferError($BufferEvent,$Events,$ClientSocket) {
         $SocketName=stream_socket_get_name($ClientSocket,true);
         $this->ClientShutDown($SocketName);
     }
